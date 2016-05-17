5d798f4b76274dd75112b171344d3f5c7f8c0fe7
ForeverGlory <foreverglory@qq.com>
2016-05-17 16:39:03 +0800

    MenuInterface extends \Knp\Menu\ItemInterface

---
+++ Model/MenuInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glory\Bundle\MenuBundle\Model;
+
+use Knp\Menu\ItemInterface;
+
+/**
+ * MenuInterface
+ * 
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+interface MenuInterface extends ItemInterface
+{
+    
+}
